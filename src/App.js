c86905817c7ff3f7528885dd2bd477f0b33a7f46
Noah Eigenfeld <noaheig@gmail.com>
2020-01-21 14:21:45 -0500

    initial app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import basketGif from './made-the-basket.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		  <h1>Binney Baskets</h1>
+      <img src={basketGif} alt="loading..." width="936" height="532"/>
+      <h2>Leaderboard</h2>
+      <table>
+        <th>Current Ranking</th>
+        <tr>Panos</tr>
+        <tr>Mamoon</tr>
+        <tr>Logan</tr>
+        <tr>Noah</tr>
+        <tr>Steve G.</tr>
+        <tr>Steve L.</tr>
+        <tr>Peter</tr>
+        <tr>Navneet</tr>
+        <tr>Xiaoyan</tr>
+        <tr>Tyler</tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
